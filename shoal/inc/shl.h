8da1a0855f1b956359c7d7b5d2147294a2356359
Stefan Kästle <stefan.kaestle@inf.ethz.ch>
2014-06-13 17:58:22 +0200

    Attempt to get replication right. Need to copy back results properly to verify

--- shoal/inc/shl.h
+++ shoal/inc/shl.h
@@ -7,15 +7,28 @@ void shl__init(void)
     printf("done\n");
 }
 
-void* shl__copy_array(void *src, size_t size, bool is_used, const char* array_name)
+static int shl__get_num_replicas(void)
 {
-    void *tmp = malloc(size);
+    return 2;
+}
+
+void** shl__copy_array(void *src, size_t size, bool is_used,
+                       bool is_ro, const char* array_name)
+{
+    int num_replicas = is_ro ? shl__get_num_replicas() : 1;
+
+    printf("array: [%-30s] copy [%c] -- replication [%c] (%d)\n", array_name,
+           is_used ? 'X' : ' ', is_ro ? 'X' : ' ', num_replicas);
+
+    void **tmp = (void**) (malloc(num_replicas*sizeof(void*)));
+
+    for (int i=0; i<num_replicas; i++)
+        tmp[i] = malloc(size);
+
     assert(tmp!=NULL);
     if (is_used) {
-        printf("[x] copying array [%s] content .. \n", array_name);
-        memcpy(tmp, src, size);
-    } else {
-        printf("[ ] copying array [%s] content .. \n", array_name);
+        for (int i=0; i<num_replicas; i++)
+            memcpy(tmp[i], src, size);
     }
     return tmp;
 }
