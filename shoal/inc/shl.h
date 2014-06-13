49d7a20eb0c8842feb360705b8cac3ce01fa502c
Stefan Kästle <stefan.kaestle@inf.ethz.ch>
2014-06-13 16:07:31 +0200

    Only copy between arrays if really needed

--- shoal/inc/shl.h
+++ shoal/inc/shl.h
@@ -7,7 +7,18 @@ void shl__init(void)
     printf("done\n");
 }
 
-void* copy_graphs(gm_graph *G, int num_att, int *sizes, void *arrays) {
+void* shl__copy_array(void *src, size_t size, bool is_used, const char* array_name)
+{
+    void *tmp = malloc(size);
+    assert(tmp!=NULL);
+    if (is_used) {
+        printf("copying array [%s] content .. \n", array_name);
+        memcpy(tmp, src, size);
+    } else {
+        printf("NOT copying array [%s] content .. \n", array_name);
+    }
+    return tmp;
 }
+
 
 #endif
