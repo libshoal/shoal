4d804fdd6b76fd17653952814c17c35cda76b0c6
Stefan Kästle <webmaster@mad-kow.de>
2014-06-14 21:37:01 +0200

    Copy back written arrays that have been allocated by parent function

--- shoal/inc/shl.h
+++ shoal/inc/shl.h
@@ -33,5 +33,34 @@ void** shl__copy_array(void *src, size_t size, bool is_used,
     return tmp;
 }
 
+void shl__copy_back_array(void **src, void *dest, size_t size, bool is_copied,
+                          bool is_ro, bool is_dynamic, const char* array_name)
+{
+    bool copy_back = true;
+    int num_replicas = is_ro ? shl__get_num_replicas() : 1;
+
+    // read-only: don't have to copy back, data is still the same
+    if (is_ro)
+        copy_back = false;
+
+    // dynamic: array was created dynamically in GM algorithm function,
+    // no need to copy back
+    if (is_dynamic)
+        copy_back = false;
+
+    printf("array: [%-30s] -- copied [%c] -- copy-back [%c] (%d)\n",
+           array_name, is_copied ? 'X' : ' ', copy_back ? 'X' : ' ',
+           num_replicas);
+
+    if (copy_back) {
+        // replicated data is currently read-only (consistency issues)
+        // so everything we have to copy back is not replicated
+        assert (num_replicas == 1);
+
+        for (int i=0; i<num_replicas; i++)
+            memcpy(dest, src[0], size);
+    }
+}
+
 
 #endif
