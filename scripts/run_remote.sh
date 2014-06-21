d964742cfac6906fd6eb93bbc6287849e8a67e65
Stefan Kästle <stefan.kaestle@inf.ethz.ch>
2014-06-21 19:07:42 +0200

    Improved script to execute measurements on remote machine

--- scripts/run_remote.sh
+++ scripts/run_remote.sh
@@ -7,11 +7,13 @@ function error() {
 
 function usage() {
 
-    echo "Usage: $0 <workload> <implementation>"
+    echo "Usage: $0 <program> <implementation>"
     exit 1
 }
 
 [[ -n "$2" ]] || usage
+
+MACHINE=sgs-r815-03
 
 # params
 # 1) <num_threads> 2) workload 3) implementation
@@ -21,27 +23,36 @@ function get_command() {
 }
 
 TMP=`mktemp`
+FTOTAL=`mktemp`
+FCOMP=`mktemp`
+echo "Files are: $FTOTAL $FCOMP"
+
 (
     . ~/.bashrc
-    echo "x y e"
+    echo "x y e" > $FTOTAL
+    echo "x y e" > $FCOMP
     for i in {6..0}
     do
-	echo -n $((2**$i))
+	echo -n $((2**$i)) >> $FTOTAL
+	echo -n $((2**$i)) >> $FCOMP
+
 	get_command $((2**$i)) $1 $2
 	echo "Executing command [${next_command}]" 1>&2
-	exec_avg ssh sgs-r820-01 "${next_command}" &> $TMP
+	exec_avg ssh $MACHINE "${next_command}" &> $TMP
 	echo "Output was:" 1>&2
-#	cat $TMP 1>&2
+	cat $TMP 1>&2
 	if [[ $2 == "ours" ]]
 	then
-	    cat $TMP | awk '/^total/ { print $2 }' | skstat.py
-	    cat $TMP | awk '/^comp/ { print $2 }' | skstat.py
+	    cat $TMP | awk '/^total/ { print $2 }' | skstat.py >> $FTOTAL
+	    cat $TMP | awk '/^comp/ { print $2 }' | skstat.py >> $FCOMP
 	fi
 
 	if [[ $2 == "theirs" ]]
 	then
-	    cat $TMP | awk -F'=' '/running time/ { print $2 }' | skstat.py
+	    cat $TMP | awk -F'=' '/running time/ { print $2 }' | skstat.py >> $FTOTAL
 	fi
     done
 )
 rm $TMP
+
+echo "Files are: $FTOTAL $FCOMP"
