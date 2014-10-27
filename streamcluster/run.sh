d47b917d3d2eaa316d13c42b4ec18f2739c27ca8
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2014-10-27 15:51:28 +0100

    Removed GM files

--- streamcluster/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#time ./streamcluster 10 20 32 4096 4096 1000  ../streamcluster/inputs/input_4k.txt output.txt `nproc`
-./streamcluster 10 20 128 0 200000 5000 /mnt/scratch/skaestle/inputs/sc_input_native output.txt `nproc`
