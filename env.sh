f4d4e48705fe26a85871d4bbe3c69a89134440c8
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2014-12-03 17:28:43 +0100

    Fixed environment

--- env.sh
+++ env.sh
@@ -6,4 +6,4 @@ export GOMP_CPU_AFFINITY=0-`nproc`
 # http://stackoverflow.com/a/246128
 PREFIX="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export LD_LIBRARY_PATH=$PREFIX/shoal:$PREFIX/contrib/numactl-2.0.9:$PREFIX/contrib/papi-5.3.0/src:$PREFIX/contrib/papi-5.3.0/src/libpfm4/lib
+export LD_LIBRARY_PATH=$PREFIX/shoal:$PREFIX/contrib/numactl-2.0.9:$PREFIX/contrib/papi-5.3.0/src:$PREFIX/contrib/papi-5.3.0/src/libpfm4/lib:$LD_LIBRARY_PATH
